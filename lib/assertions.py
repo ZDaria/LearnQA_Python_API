efdfcf3b1f355804fc7522f66e403da0859dbe8c
Zavgorodneva Daria <d210927@yandex.ru>
2023-02-24 17:06:17 +0300

    - were added validations to test_user_get.py file (EX16)

--- lib/assertions.py
+++ lib/assertions.py
@@ -55,3 +55,15 @@ class Assertions:
 
         assert name not in response_as_dict, \
             f"Response JSON shouldn't have key '{name}'. But it is present."
+
+    @staticmethod
+    def assert_json_has_not_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is in JSON format. " \
+                          f"Response text is '{response.text}'."
+
+        for name in names:
+            assert name not in response_as_dict, \
+                f"Response JSON shouldn't have key '{name}'. But it is present."
--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -31,3 +31,29 @@ class TestUserGet(BaseCase):
 
         expected_keys = ["username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(resp2, expected_keys)
+
+    def test_user_details_auth_other_user(self):
+        user1_data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        user2_data = {
+            'email': 'learnqa02242023164213@example.com',
+            'password': '123'
+        }
+
+        resp1 = MyRequests.post("/user/login", data=user1_data)
+        user_id_from_auth_method = self.get_json_value(resp1, "user_id")
+
+        resp2 = MyRequests.post("/user/login", data=user2_data)
+        auth_sid = self.get_cookie(resp2, "auth_sid")
+        token = self.get_header(resp2, "x-csrf-token")
+
+        resp3 = MyRequests.get(f"/user/{user_id_from_auth_method}",
+                               headers={"x-csrf-token": token},
+                               cookies={"auth_sid": auth_sid})
+
+        expected_keys = ["username"]
+        unexpected_keys = ["email", "firstName", "lastName"]
+        Assertions.assert_json_has_keys(resp3, expected_keys)
+        Assertions.assert_json_has_not_keys(resp3, unexpected_keys)
