d76405c4384c8851c80affa9933ede4351dbbd93
Zavgorodneva Daria <d210927@yandex.ru>
2023-02-24 15:01:01 +0300

    - were added Allure report methods

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -2,8 +2,10 @@ import pytest
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from lib.my_requests import MyRequests
+import allure
 
 
+@allure.epic("Authorization cases")
 class TestUserAuth(BaseCase):
     exclude_params = [
         ("no_cookie"),
@@ -23,6 +25,8 @@ class TestUserAuth(BaseCase):
         self.user_id_from_auth_method = self.get_json_value(response1,
                                                             "user_id")
 
+    @allure.description("This test successfully authorize by email "
+                        "and password")
     def test_auth_user(self):
 
         response_2 = MyRequests.get("/user/auth",
@@ -35,6 +39,8 @@ class TestUserAuth(BaseCase):
                                              "equal to the user id from "
                                              "check method.")
 
+    @allure.description("This test checks authorization status w/o sending "
+                        "auth cookie or token")
     @pytest.mark.parametrize('condition', exclude_params)
     def test_negative_auth_check(self, condition):
         if condition == 'no_cookie':
