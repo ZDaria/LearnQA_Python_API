ac048fe44786ab308fd5982e557059eec53be72e
Zavgorodneva Daria <d210927@yandex.ru>
2023-02-24 13:16:17 +0300

    lesson examples were added

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,5 +1,5 @@
 import json.decoder
-
+from datetime import datetime
 from requests import Response
 
 
@@ -25,3 +25,18 @@ class BaseCase:
         assert name in resp_as_dict, \
             f"Response JSON does not have key '{name}'."
         return resp_as_dict[name]
+
+    def prepare_registration_data(self, email=None):
+        if email is None:
+            base_part = 'learnqa'
+            domain = 'example.com'
+            random_part = datetime.now().strftime('%m%d%Y%H%M%S')
+            email = f"{base_part}{random_part}@{domain}"
+
+        return {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
---
+++ tests/test_user_register.py
@@ -0,0 +1,27 @@
+import datetime
+
+import requests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+
+url = "https://playground.learnqa.ru/ajax/api/user"
+
+
+class TestUserRegister(BaseCase):
+    def test_create_user_successfully(self):
+        data = self.prepare_registration_data()
+        response = requests.post(url, data=data)
+        Assertions.assert_code_status(response, 200)
+        # b'{"id":"62915"}'
+        Assertions.assert_json_has_key(response, 'id')
+
+    def test_create_user_with_existing_email(self):
+        email = 'vinkotov@example.com'
+        data = self.prepare_registration_data(email=email)
+
+        response = requests.post(url, data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode('utf-8') == \
+               f"Users with email '{email}' already exists", \
+               f"Unexpected response content: {response.content}"
