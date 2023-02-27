c0351bb6746c4716fbbfa3a2ae607389080996ff
Zavgorodneva Daria <d210927@yandex.ru>
2023-02-27 16:47:20 +0300

    - were added allure tags for test (EX19)

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -1,16 +1,22 @@
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+import allure
 
 
+@allure.title("Edit user validation")
 class TestUserEdit(BaseCase):
+    @allure.testcase("TEST-001")
+    @allure.title("Make sure, that it is possible to edit user (Just created)")
+    @allure.story("TEST-002 Endpoint /user")
     def test_edit_just_created_user(self):
         # Register
         register_data = self.prepare_registration_data()
-        response1 = MyRequests.post("/user", data=register_data)
 
-        Assertions.assert_code_status(response1, 200)
-        Assertions.assert_json_has_key(response1, 'id')
+        with allure.step("Create new user"):
+            response1 = MyRequests.post("/user", data=register_data)
+            Assertions.assert_code_status(response1, 200)
+            Assertions.assert_json_has_key(response1, 'id')
 
         email = register_data['email']
         first_name = register_data['firstName']
@@ -24,8 +30,9 @@ class TestUserEdit(BaseCase):
             'password': password
         }
 
-        response2 = MyRequests.post("/user/login", data=login_data)
-
+        with allure.step("Auth with new user"):
+            response2 = MyRequests.post("/user/login", data=login_data)
+            Assertions.assert_code_status(response2, 200)
         auth_sid = self.get_cookie(response2, 'auth_sid')
         token = self.get_header(response2, 'x-csrf-token')
 
@@ -37,17 +44,20 @@ class TestUserEdit(BaseCase):
                                    headers={"x-csrf-token": token},
                                    cookies={"auth_sid": auth_sid},
                                    data={'firstName': new_name})
-
-        Assertions.assert_code_status(response3, 200)
+        with allure.step("Make sure, that edite were applied: "
+                         "status code 200. "):
+            Assertions.assert_code_status(response3, 200)
 
         # GET
-        response4 = MyRequests.get(f"/user/{user_id}",
-                                   headers={"x-csrf-token": token},
-                                   cookies={"auth_sid": auth_sid})
 
-        Assertions.assert_json_value_by_name(response4, 'firstName', new_name,
-                                             "Wrong name of the user after "
-                                             "edit.")
+        with allure.step("Make sure, that changes were saved"):
+            response4 = MyRequests.get(f"/user/{user_id}",
+                                       headers={"x-csrf-token": token},
+                                       cookies={"auth_sid": auth_sid})
+            Assertions.assert_json_value_by_name(response4, 'firstName',
+                                                 new_name,
+                                                 "Wrong name of the user "
+                                                 "after edit.")
 
     def test_edit_not_authorized_user(self):
         # Register
@@ -57,9 +67,6 @@ class TestUserEdit(BaseCase):
         Assertions.assert_code_status(response1, 200)
         Assertions.assert_json_has_key(response1, 'id')
 
-        email = register_data['email']
-        first_name = register_data['firstName']
-        password = register_data['password']
         user_id = self.get_json_value(response1, 'id')
 
         # EDIT
